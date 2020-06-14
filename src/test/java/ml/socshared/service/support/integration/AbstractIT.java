2371ad3153cd02122d72785a3c7959741a2e48ac
aleksey-ageev <ageevav97@gmail.com>
2020-06-14 14:30:45 +0300

    Отключены тесты

--- src/test/java/ml/socshared/service/support/integration/AbstractIT.java
+++ src/test/java/ml/socshared/service/support/integration/AbstractIT.java
@@ -1,22 +1,22 @@
-package ml.socshared.service.support.integration;
-
-import lombok.Getter;
-import org.junit.jupiter.api.extension.ExtendWith;
-import org.springframework.boot.test.context.SpringBootTest;
-import org.springframework.boot.web.server.LocalServerPort;
-import org.springframework.test.context.ActiveProfiles;
-import org.springframework.test.context.junit.jupiter.SpringExtension;
-
-import static org.springframework.boot.test.context.SpringBootTest.WebEnvironment.RANDOM_PORT;
-import static ml.socshared.service.support.config.Constants.TEST_PROFILE;
-
-@ExtendWith(SpringExtension.class)
-@SpringBootTest(webEnvironment = RANDOM_PORT)
-@ActiveProfiles(TEST_PROFILE)
-public abstract class AbstractIT {
-
-    @Getter
-    @LocalServerPort
-    private int localServerPort;
-}
-
+//package ml.socshared.service.support.integration;
+//
+//import lombok.Getter;
+//import org.junit.jupiter.api.extension.ExtendWith;
+//import org.springframework.boot.test.context.SpringBootTest;
+//import org.springframework.boot.web.server.LocalServerPort;
+//import org.springframework.test.context.ActiveProfiles;
+//import org.springframework.test.context.junit.jupiter.SpringExtension;
+//
+//import static org.springframework.boot.test.context.SpringBootTest.WebEnvironment.RANDOM_PORT;
+//import static ml.socshared.service.support.config.Constants.TEST_PROFILE;
+//
+//@ExtendWith(SpringExtension.class)
+//@SpringBootTest(webEnvironment = RANDOM_PORT)
+//@ActiveProfiles(TEST_PROFILE)
+//public abstract class AbstractIT {
+//
+//    @Getter
+//    @LocalServerPort
+//    private int localServerPort;
+//}
+//
